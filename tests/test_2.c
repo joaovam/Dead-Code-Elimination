94b5ebb0e46334ac1270b1cd397b905762280c78
joaovam <amorimvictorjoao3@gmail.com>
2024-06-20 10:43:08 -0300

    add tests

---
+++ tests/test_2.c
@@ -0,0 +1,7 @@
+int m(){
+    for(int i = 0; i < 5;i++){
+        if(i==10){
+            printf("Should be removed =)");
+        }
+    }
+}
---
+++ tests/test_3.c
@@ -0,0 +1,11 @@
+int bar(){
+    int i = 0;
+
+    if(i > 100){
+        
+        for(int x = 0; x < 100; x++){
+            printf("%d\n", x);
+        }
+
+    }
+}
