d43c7ab1e8b2d862c7fe4938b3df0c4463902570
joaovam <amorimvictorjoao3@gmail.com>
2024-06-20 10:08:08 -0300

    create run script

--- runDCE.sh
+++ runDCE.sh
@@ -3,38 +3,29 @@
 
 if [ $# -lt 1 ]
 then
-    echo Syntax: run_RA file.c
+    echo Syntax: ./runDCE.sh file.c
     exit 1
 else
-
+    LLVM_INSTALL_DIR="/usr/lib/llvm-8/build"
     suffix=".dylib"
     if [[$("uname -s") == "Linux" ]]; then
         suffix=".so"
     fi
 
-    CLANG="/usr/bin/clang++"
-    OPT="/usr/bin/opt"
-    DCE_LIB="/home/joaovam/projects/courses/Dead-Code-Elimination/lib/DeadCodeElimination${suffix}"
+    CLANG=$LLVM_INSTALL_DIR/Release/bin/clang
+    OPT=$LLVM_INSTALL_DIR/Release/bin/opt
+    DCE_LIB="/lib/DeadCodeElimination${suffix}"
 
     file_name=$1
     base_name=$(basename $1 .c)
     program_ll="$base_name.ll"
     
 
-    # Produce a bytecode file:
-    $CLANG $file_name -o $program_ll  -Xclang -disable-O0-optnone -S -emit-llvm
+    $CLANG -Xclang -disable-O0-optnone -S -emit-llvm $1 -o $INPUT_IR
+    $OPT -instnamer -mem2reg -break-crit-edges -S $INPUT_IR -o $INPUT_IR
+    $OPT -load build/lib/libDeadCodeElimination.so -vssa -S $INPUT_IR -o $INPUT_IR
+    $OPT -dot-cfg $INPUT_IR -cfg-dot-filename-prefix=./input/before -disable-output
 
-    # Convert the bytecode to SSA form, find nice names for variables and
-    # break critical edges:
-    $OPT -instnamer -mem2reg -break-crit-edges $program_ll -S -o $program_ll
-
-    # Break live ranges after conditionals to improve precision:
-    $OPT -load $DCE_LIB -vssa -dead-code-elimination  -simplifycfg -stats -S -o=$program_ll < $program_ll
-
-    # Producing a dot file for the vssa version of the bytecode file:
-    $OPT -dot-cfg $program_ll -disable-output
-
-    # Remove the temporary files produced. We keep the '.s' file if the option
-    # -s was passed in the command line.
-    
+    $OPT -stats -load build/lib/libDeadCodeElimination.so -dead-code-elimination -S $INPUT_IR -o $INPUT_IR
+    $OPT -dot-cfg $INPUT_IR -cfg-dot-filename-prefix=./input/after -disable-output
 fi
