f2b01fbbe305ab6a818c588ea0f2f7abf389dae7
joaovam <amorimvictorjoao3@gmail.com>
2024-06-20 11:03:58 -0300

    update tests

--- tests/test_3.c
+++ tests/test_3.c
@@ -1,7 +1,7 @@
 int bar(){
     int i = 0;
 
-    if(i > 100){
+    if(i >= 100){
         
         for(int x = 0; x < 100; x++){
             printf("%d\n", x);
---
+++ tests/test_5.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int d(){
+    int i = 100;
+
+    if(i <= 5){
+        for(int j = 0; j < 100;j++){
+            int z = j;
+            printf("%d", z);
+        }
+    }
+}
